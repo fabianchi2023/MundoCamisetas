473b5e6e764999f4aad7abc6696c861fdfc84201
fabianchi <fabiobianchicaseros@gmail.com>
2024-05-30 15:02:01 -0300

    Agregado del Cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ItemListContainer from "./components/ItemListContainer"
 import NavBar from "./components/NavBar"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import CartContextProvider from "./context/cartContext"
+import Cart from "./components/Cart"
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<ItemListContainer />} />
           <Route path="/category/:id" element={<ItemListContainer />} />
           <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="/cart" element={<Cart/>} />
         </Routes>
       </BrowserRouter>
     </CartContextProvider>
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom"
 import bag from "../assets/img/bag.svg"
 
 const CartWidget = () => {
     return (
         <div>
-            <button type="button" className="btn btn-outline-primary position-relative ">
+            <Link to={"/cart"} className="btn btn-outline-primary position-relative">
                 <img src={bag} alt="carrito" />
                 <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-dark">1
                 </span>
-            </button>
+            </Link>
         </div>
     )
 }
